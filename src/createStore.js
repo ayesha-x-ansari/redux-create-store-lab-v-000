c89489e07ed1d194de6becca55977624e40f6156
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-11-02 12:56:21 +0000

    automatically backed up by learn

--- src/createStore.js
+++ src/createStore.js
@@ -1,5 +1,19 @@
 export default function createStore(reducer) {
   // add your code here
+  let state;
+
+  function dispath (reducer){
+    state = reducer(state, action)
+      render()
+    }
+  function getstate(){
+    return state;
+   }
+
+   dispatch({@@init})
+
+   return{dispatch,
+    getstate}   
 }
 
 function render() {
